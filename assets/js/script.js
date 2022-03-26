23eb71d02da9bed89a78cbaf8c00c5aec04dbe47
Koby1705 <koby1705@gmail.com>
2022-03-26 23:46:00 +0000

    Fix small issue with 1 constant

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,6 @@ const leftTopQuadrant = document.getElementsByClassName("left-top-quadrant");
 const rightTopQuadrant = document.getElementsByClassName("right-top-quadrant");
 const leftBottomQuadrant = document.getElementsByClassName("left-bottom-quadrant");
 const rightBottomQuadrant = document.getElementsByClassName("right-bottom-quadrant")
+const onButton = document.getElementById("on");
 const goButton = document.getElementById("go");
-const powerButton = document.getElementById("power");
 const levelCounter = document.getElementById("turn");
