0edfd4d35c972fadd915e68e9f11f8022ee82422
Koby1705 <koby1705@gmail.com>
2022-03-26 23:04:19 +0000

    Declare const variables necessary for interaction with the game elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-
+// Declare 7 constant variables on global scope to get access to specific elements with which we are going to interact
+const leftTopQuadrant = document.getElementsByClassName("left-top-quadrant");
+const rightTopQuadrant = document.getElementsByClassName("right-top-quadrant");
+const leftBottomQuadrant = document.getElementsByClassName("left-bottom-quadrant");
+const rightBottomQuadrant = document.getElementsByClassName("right-bottom-quadrant")
+const goButton = document.getElementById("go");
+const powerButton = document.getElementById("power");
+const levelCounter = document.getElementById("turn");
