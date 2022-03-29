f645625f4779999694575b7ee9bad903567f2518
Koby1705 <koby1705@gmail.com>
2022-03-29 15:41:45 +0000

    Store variables globally for cleaner code & change comp flashes to black

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,17 @@ const onButton = document.getElementById("on");
 const goButton = document.getElementById("go");
 const levelCounter = document.getElementById("turn");
 
+let intervalId;
+let win;
+let good;
+let compSequence = [];
+let humanSequence = [];
+let compTurn;
+let flash;
+let level;
+
 // EventListener for power button to turn the game on
 onButton.addEventListener ("click", (event) => {
-    let on;
-    let intervalId;
-
     if (onButton.checked == true) {
       on = true;
       levelCounter.innerHTML = "HI!";
@@ -26,9 +32,6 @@ onButton.addEventListener ("click", (event) => {
 // EventListener for go button to start the game using start game function if the game is on
 // and also if player wins the game
 goButton.addEventListener ("click", (event) => {
-    let on = true;
-    let win = true;
-
     if (on == true || win == true) {
         startGame ();
     }
@@ -38,35 +41,29 @@ goButton.addEventListener ("click", (event) => {
  *  The main game loop called when the "go" button is pressed
  */ 
 function startGame () {
-  let win = false;
-  let level = 1;
-  let flash = 0;
-  let intervalId = 0;
-  let good = true;
-  let compSequence = [];
-  let humanSequence = [];
+  win = false;
+  level = 1;
+  flash = 0;
+  intervalId = 0;
+  good = true;
+  compSequence = [];
+  humanSequence = [];
   levelCounter.innerHTML = 1;
 
   for (let i = 0; i < 12; i++) {
     compSequence.push(Math.floor(Math.random() * 4) + 1);
   }
-  let compTurn = true;
+  compTurn = true;
 
   intervalId = setInterval(gameTurn, 1000);
 };
 
 /**
- * When invoked it flashes the colors every 350ms in random order & increments the level by 1
+ * When invoked it's going to stop flashes of the the colors for 350ms in random order & increments the level by 1
  * when player guess the correct order
  */
 function gameTurn () {
-  let on;
   on = false;
-  let flash;
-  let level;
-  let intervalId;
-  let compTurn;
-  let compSequence = [];
 
   if (flash == level) {
     on = true;
@@ -99,20 +96,20 @@ function clearColor () {
 };
 
 
-// 4 functions with which computer flashes the colors at random order making them transparent
+// 4 functions with which computer flashes the colors at random order making them black
 function firstQuadrant () {
-  leftTopQuadrant[0].style.backgroundColor = "transparent"
+  leftTopQuadrant[0].style.backgroundColor = "black";
 };
 
 function secondQuadrant () {
-  rightTopQuadrant[0].style.backgroundColor = "transparent"
+  rightTopQuadrant[0].style.backgroundColor = "black";
 };
 
 function thirdQuadrant () {
-  leftBottomQuadrant[0].style.backgroundColor = "transparent"
+  leftBottomQuadrant[0].style.backgroundColor = "black";
 };
 
 function fourthQuadrant () {
-  rightTopQuadrant[0].style.backgroundColor = "transparent"
+  rightBottomQuadrant[0].style.backgroundColor = "black";
 };
 
